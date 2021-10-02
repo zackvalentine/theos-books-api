da857f4d7ac9db8d840298bde42c083c25294045
Zack <zack.valentine22@gmail.com>
2021-10-01 21:43:36 -0500

    CORS

--- src/main/java/com/doestheohavethisbook/controllers/HelloController.java
+++ src/main/java/com/doestheohavethisbook/controllers/HelloController.java
@@ -2,6 +2,7 @@ package com.doestheohavethisbook.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<String> hello() {
         return new ResponseEntity<>("Hello, World!", HttpStatus.OK);
